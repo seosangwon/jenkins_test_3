d5b60c91fe216a820f7cb52b06314dd9283737f2
jhs512 <jangka512@gmail.com>
2023-12-20 09:41:47 +0900

    작업

--- src/test/java/com/ll/deploy231218/Deploy231218ApplicationTests.java
+++ src/test/java/com/ll/deploy231218/Deploy231218ApplicationTests.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class Deploy231218ApplicationTests {
     @Test
     void contextLoads() {
+        assertThat(1).isEqualTo(2);
     }
 }
